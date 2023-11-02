cbc735f94c2ff093c47280daec9dfb614748b80c
Gabriel <gabriel@mazure.fr>
2023-11-02 19:01:36 +0100

    Rajout des couleurs et affichage de la main

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function initialisation_deck() {
     let deckBase = [];
     for (let i=0; i<valeurs.length; i++) {
         for (let j=0; j<couleurs.length; j++) {
-            deckBase.push(couleurs[j] + valeurs[i]);
+            deckBase.push("<div class='" + couleurs[j] + "'>" + couleurs[j] + valeurs[i] + "</div>");
         }
     }
     console.log(deckBase);
@@ -20,12 +20,16 @@ let indice;
 let defausse = [];
 
 function piocher(){
-    indice = Math.round(Math.random() * pioche.length);
+    if (pioche.length == 0){
+        refaire_deck()
+    }
+    indice = Math.floor(Math.random() * pioche.length);
     main.push(pioche[indice]);
     pioche.splice(indice, 1);
     if (main.length > 5) {
         defausse.push(main.shift());
     }
+    montrer_main()
     console.log(main);
     console.log(defausse);
 }
@@ -33,4 +37,12 @@ function piocher(){
 function refaire_deck() {
     pioche = pioche.concat(defausse);
     defausse = [];
+}
+
+function montrer_main() {
+    document.getElementById("carte1").innerHTML = main[0]
+    document.getElementById("carte2").innerHTML = main[1]
+    document.getElementById("carte3").innerHTML = main[2]
+    document.getElementById("carte4").innerHTML = main[3]
+    document.getElementById("carte5").innerHTML = main[4]
 }
